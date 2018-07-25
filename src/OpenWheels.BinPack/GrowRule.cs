c3d15551ac80d67fd296c2e831775bb93d2c8ef5
Jesse Gielen <jessegielen@hotmail.com>
2018-07-25 12:08:19 +0200

    Replace BinPack submodule with subtree

---
+++ src/OpenWheels.BinPack/GrowRule.cs
@@ -0,0 +1,25 @@
+﻿namespace OpenWheels.BinPack
+{
+    /// <summary>
+    /// How to grow the bin when a rectangel can't be placed.
+    /// </summary>
+    public enum GrowRule
+    {
+        /// <summary>
+        /// Don't grow the bin, throw an exception when full.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Grow the bins width.
+        /// </summary>
+        Width,
+        /// <summary>
+        /// Grow the bins height.
+        /// </summary>
+        Height,
+        /// <summary>
+        /// Alternate growing the bins width and height. Starts with width.
+        /// </summary>
+        Both
+    }
+}
---
+++ src/OpenWheels.BinPack/MaxRectsHeuristic.cs
@@ -0,0 +1,33 @@
+﻿namespace OpenWheels.BinPack
+{
+    /// <summary>
+    /// Heuristic method to use to choose the rectangles positions.
+    /// </summary>
+    public enum MaxRectsHeuristic
+    {
+        /// <summary>
+        /// Best Short Side Fit: Positions the rectangle against the short side of a free rectangle into which it fits the best.
+        /// </summary>
+        Bssf,
+
+        /// <summary>
+        /// Best Long Side Fit: Positions the rectangle against the long side of a free rectangle into which it fits the best.
+        /// </summary>
+        Blsf,
+
+        /// <summary>
+        /// Best Area Fit: Positions the rectangle into the smallest free rect into which it fits.
+        /// </summary>
+        Baf,
+
+        /// <summary>
+        /// Bottom Left (Tetris method): Each rectangle is placed to a position (possibly rotating it) where its top side lies as low as possible.
+        /// </summary>
+        Bl,
+
+        /// <summary>
+        /// Contact Point: Choosest the placement where the rectangle touches other rects as much as possible.
+        /// </summary>
+        RectContactPointRule
+    }
+}
